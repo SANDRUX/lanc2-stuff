40a2546556544b57d28955d4bf6f7390eee1e637
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-02-11 13:46:51 +0400

    added command line indexing

--- lab02a.cpp
+++ lab02a.cpp
@@ -14,43 +14,37 @@ std::string convertToString(char *a, int size)
     return s;
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     int size = argc - 1;
 
-    string * args;
+    string *args;
 
     cout << "The address of args when declaring is: " << args << endl;
-    cout << "The value of args when declaring is: " << *args << endl;
-    
 
     args = new string[size];
 
     cout << "The address of args when allocating dynamic array is: " << args << endl;
-    cout << "The value of args when allocating dynamic array is: " << *args << endl;
 
     for (int i = 0; i < size; i++)
     {
-        args[i] = argv[i];
+        args[i] = argv[i + 1];
     }
 
     cout << "The address of args when filling the array is: " << args << endl;
-    cout << "The value of args when filling the array is: " << *args << endl;
 
     for (int i = 0; i < size; i++)
     {
         cout << args[i] << endl;
     }
-    
+
     delete[] args;
 
     cout << "The address of args when deleting the array is: " << args << endl;
-    cout << "The value of args when deleting the array is: " << *args << endl;
 
     args = NULL;
 
     cout << "The address of args when assigning it to a NULL is: " << args << endl;
-    cout << "The value of args when assigning it to a NULL is: " << *args << endl;
 
     return 0;
 }
