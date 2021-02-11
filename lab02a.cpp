60b4c0060f4439cc1c166ddd1caae757bf71dc44
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-02-11 13:40:35 +0400

    first task accomplished

---
+++ lab02a.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+std::string convertToString(char *a, int size)
+{
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++)
+    {
+        s = s + a[i];
+    }
+    return s;
+}
+
+int main(int argc, char * argv[])
+{
+    int size = argc - 1;
+
+    string * args;
+
+    cout << "The address of args when declaring is: " << args << endl;
+    cout << "The value of args when declaring is: " << *args << endl;
+    
+
+    args = new string[size];
+
+    cout << "The address of args when allocating dynamic array is: " << args << endl;
+    cout << "The value of args when allocating dynamic array is: " << *args << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        args[i] = argv[i];
+    }
+
+    cout << "The address of args when filling the array is: " << args << endl;
+    cout << "The value of args when filling the array is: " << *args << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << args[i] << endl;
+    }
+    
+    delete[] args;
+
+    cout << "The address of args when deleting the array is: " << args << endl;
+    cout << "The value of args when deleting the array is: " << *args << endl;
+
+    args = NULL;
+
+    cout << "The address of args when assigning it to a NULL is: " << args << endl;
+    cout << "The value of args when assigning it to a NULL is: " << *args << endl;
+
+    return 0;
+}
